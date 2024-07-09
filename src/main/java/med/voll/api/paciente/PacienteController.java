1c2b0be526e255d23c57c9dcebaa3fc0cf756a93
Luiz Aquino <quinnpw@live.com>
2024-07-09 15:10:31 -0300

    criado entidade paciente e pacienteRepository e feito validações

--- src/main/java/med/voll/api/paciente/PacienteController.java
+++ src/main/java/med/voll/api/paciente/PacienteController.java
@@ -1,5 +1,8 @@
 package med.voll.api.paciente;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("pacientes")
 public class PacienteController {
+    @Autowired
+    private PacienteRepository repository;
 
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println("dados recebido: " +dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
     }
 }
