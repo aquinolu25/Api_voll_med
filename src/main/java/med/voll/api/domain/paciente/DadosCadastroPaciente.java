968e1d06397a36986e667dc2068a2c461ef62528
Luiz Aquino <quinnpw@live.com>
2024-07-23 14:36:57 -0300

    adicionado mensagens personalizadas no BeanValidation, nas classes DadosCadastroMedico e DadosCadastroPaciente.

--- src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
@@ -1,22 +1,24 @@
 package med.voll.api.domain.paciente;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroPaciente(
-        @NotBlank
+        @NotBlank(message = "Nome do paciente é obrigatório")
         String nome,
-        @NotBlank
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Formato do email está inválido")
         String email,
-        @NotBlank
+        @NotBlank(message = "O número de telefone é obrigatório")
         String telefone,
-        @NotBlank
-        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
+        @NotBlank(message = "O número do CPF é obrigatório")
+        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}", message = "Formato do CPF está inválido")
         String cpf,
-        @NotNull
+        @NotNull(message = "Dados do endereço são obrigatórios")
         @Valid
         DadosEndereco endereco) {
 }
