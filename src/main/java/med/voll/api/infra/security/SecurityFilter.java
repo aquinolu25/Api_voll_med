e4b8039a43c9efc7f767fd623f34c21a6c15e556
Luiz Aquino <quinnpw@live.com>
2024-07-25 15:00:57 -0300

    criado metodo de recuperação do token, onde quando não enviado é lançado uma exception

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -13,6 +13,18 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+        
         filterChain.doFilter(request, response);
     }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("O token não foi enviado, ou está incorreto!");
+        }
+        return authorizationHeader.replace("Bearer", "");
+    }
+
+
 }
